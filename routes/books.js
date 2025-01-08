0dc2fc23a90fa6bd9c9f9dca60aaf4d7c7071930
piyush-circuitstream <piyush@circuitstream.ca>
2025-01-07 21:10:09 -0700

    Added initial code for inventory exercise

---
+++ routes/books.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Get list of books
+router.get('/', (req, res) => {
+    // Logic is here
+});
+
+//Add a new book
+router.post('/', (req, res) => {
+    // Logic is here
+});
+
+//Update an existing book
+router.put('/', (req, res) => {
+    // Logic is here
+});
+
+//Delete a book
+router.delete('/', (req, res) => {
+    // Logic is here
+});
+
+module.exports = router;
