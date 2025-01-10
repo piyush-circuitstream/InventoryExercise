8c043fab4a88abd7b243bf4eafeb71a7551202f6
piyush-circuitstream <piyush@circuitstream.ca>
2025-01-09 20:00:56 -0700

    Added model and DB connection changes

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const app = express();
 const PORT = 3000;
 const bookRouter = require('./routes/books');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true })); //Requried to parse the incoming form data which is in url-encoded form. 
 app.use(express.json());
 
 app.use('/', bookRouter);
 
-//Need to write the DB connection here...
+async function startApp() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/inventory');
+    } catch (err) {
+        console.error('Error on connecting to DB: ', err);
+    }
 
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+startApp();
